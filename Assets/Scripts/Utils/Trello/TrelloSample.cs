44a11f2f1a9b8948423f3947e594a83f08b13e10
やまげん <dqx_2527@yahoo.co.jp>
2018-04-19 17:38:35 +0900

    trelloへのカード追加機能

---
+++ Assets/Scripts/Utils/Trello/TrelloSample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util.Trello;
+public class TrelloSample : MonoBehaviour
+{
+
+    private IEnumerator Start()
+    {
+        var manager = TrelloManager.Instance;
+        yield return new WaitUntil(() => !manager.IsLoading);
+
+        // カードの作成
+        var card = manager.NewCard();
+        card.name = "a";
+        card.desc = "";
+
+        manager.SendCardScreenShot(card);
+    }
+}
