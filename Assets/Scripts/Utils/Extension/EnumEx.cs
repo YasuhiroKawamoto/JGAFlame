97bfeb1f720c0ac274e461d5110da4fc35e217b5
DESKTOP-R1GNS6C\yamak <dqx_2527@yahoo.co.jp>
2018-04-24 00:09:07 +0900

    列挙型の拡張とマテリアルの拡張

---
+++ Assets/Scripts/Utils/Extension/EnumEx.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Extensions
+{
+	public static class EnumEx
+	{
+		/// <summary>
+		/// 現在のインスタンスで 1 つ以上のビット フィールドが設定されているかどうか判定
+		/// </summary>
+		public static bool HasFlag(this Enum self, Enum flag)
+		{
+			if (self.GetType() != flag.GetType())
+			{
+				throw new ArgumentException("flag がインスタンスの型と異なっています。");
+			}
+
+			var selfValue = Convert.ToUInt64(self);
+			var flagValue = Convert.ToUInt64(flag);
+
+			return (selfValue & flagValue) == flagValue;
+		}
+	}
+}
---
+++ Assets/Scripts/Utils/Extension/MaterialEx.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+namespace Extensions
+{
+	public static class MaterialEx
+	{
+		/// <summary>
+		/// 透明度を設定します
+		/// </summary>
+		public static void SetAlpha(this Material self, float a)
+		{
+			var color = self.color;
+			color.a = a;
+			self.color = color;
+		}
+	}
+}
