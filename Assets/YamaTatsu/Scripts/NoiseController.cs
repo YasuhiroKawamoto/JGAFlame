151d27948208bcbd52cb5b1e8fcd4c679f4695b0
YamaTatsu8 <tatsu8fate@icloud.com>
2018-04-27 11:15:31 +0900

    画像をゆがませるシェーダーの作成　PulseNoise クリックしたときに画像をゆがませる RandomNoise 砂嵐　未完成

---
+++ Assets/YamaTatsu/Scripts/NoiseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoiseController : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator GeneratePulseNoise()
+    {
+        for(int i = 0; i <=180; i += 30)
+        {
+            //歪みの変更
+            GetComponent<Image>().material.SetFloat("_Amount", 0.2f * Mathf.Sin(i * Mathf.Deg2Rad));
+            yield return null;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(GeneratePulseNoise());
+        }
+		
+	}
+}
